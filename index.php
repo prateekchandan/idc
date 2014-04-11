caa2d5622b8f48fd9f60aa5d1023628d4f036614
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2014-04-11 02:02:16 +0000

    ckjbncjbhbajh

--- index.php
+++ index.php
@@ -57,11 +57,12 @@
 					  <div class="form-group">
 					    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter your email address">
 					  </div>
-					  <button type="submit" class="btn btn-warning btn-lg">Help Me!</button>
+					  <button type="submit" class="btn btn-primary btn-lg">Help Me!</button>
 					</form>					
 				</div><!-- /col-lg-6 -->
 				<div class="col-lg-6">
-					<img class="img-responsive" src="assets/img/ipad-hand.png" alt="">
+					<img class="img-responsive" src="assets/img/book_1.png" height="70%" width="70%" alt="">
+					<!--Book by John Caserta from The Noun Project-->
 				</div><!-- /col-lg-6 -->
 				
 			</div><!-- /row -->
@@ -72,29 +73,74 @@
 	<div class="container">
 		<div class="row mt centered">
 			<div class="col-lg-6 col-lg-offset-3">
-				<h1>Your Landing Page<br/>Looks Wonderful Now.</h1>
-				<h3>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</h3>
+				<h1>Your Career Path<br/>Looks Clearer Now.</h1>
+				<h3>People have bee confused over which career path to take since far too long. It's time to change that.</h3>
 			</div>
 		</div><!-- /row -->
 		
 		<div class="row mt centered">
 			<div class="col-lg-4">
-				<img src="assets/img/ser01.png" width="180" alt="">
-				<h4>1 - Browser Compatibility</h4>
-				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.</p>
+				<img src="assets/img/books_2.png" width="180" alt=""><!--Book by Derrick Snider from The Noun Project-->
+				<h4>1 - Your Academic Interests</h4>
+				<form align="left" style="padding-left:25%;">
+				<input type="checkbox" name="Engineering" value="Engineering">Engineering<br>
+				<input type="checkbox" name="CS" value="CS">Computer Science <br>
+				<input type="checkbox" name="Aero" value="Aero">Aerospace <br>
+				<input type="checkbox" name="Medical" value="Medical">Medical Sciences <br>
+				<input type="checkbox" name="Arts" value="Arts">Arts<br>
+				<input type="checkbox" name="Commerce" value="Commerce">Commerce <br>
+				<input type="checkbox" name="dev" value="Other">Development <br>
+				<input type="checkbox" name="agri" value="Other">Agriculture <br>
+				<input type="checkbox" name="soc" value="Other">Social <br>
+				<input type="checkbox" name="code" value="Other">Coding <br>
+				<input type="checkbox" name="math" value="Other">Mathematics <br>
+				<input type="checkbox" name="ps" value="Other">Pure Sciences <br>
+				<input type="checkbox" name="ass" value="Other">Applied Sciences <br>
+				<input type="checkbox" name="Other" value="Other">Other
+				</form>
 			</div><!--/col-lg-4 -->
 
 			<div class="col-lg-4">
-				<img src="assets/img/ser02.png" width="180" alt="">
-				<h4>2 - Email Campaigns</h4>
-				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.</p>
+				<img src="assets/img/act.png" width="180" alt=""><!--Basketball by Anthony Hamill from The Noun Project-->
+				<h4>2 - Activities You Love</h4>
+				<form align="left" style="padding-left:25%;">
+				<input type="checkbox" name="Film" value="Film">Film<br>
+				<input type="checkbox" name="Music" value="Music">Music Ensembles <br>
+				<input type="checkbox" name="News" value="News">Student Newspaper <br>
+				<input type="checkbox" name="choral" value="choral">Choral Groups <br>
+				<input type="checkbox" name="march" value="march">Marching Band<br>
+				<input type="checkbox" name="tel" value="tel">Television Station<br>
+				<input type="checkbox" name="tech" value="tech">Technical Activities <br>
+				<input type="checkbox" name="fb" value="fb">Football <br>
+				<input type="checkbox" name="basky" value="basky">Basketball <br>
+				<input type="checkbox" name="cricket" value="cri">Cricket <br>
+				<input type="checkbox" name="pai" value="pai">Painting <br>
+				<input type="checkbox" name="ph" value="ph">Photography <br>
+				<input type="checkbox" name="ws" value="ws">Water Sports <br>
+				<input type="checkbox" name="int" value="int">International Competitions
+				</form>
 
 			</div><!--/col-lg-4 -->
 
 			<div class="col-lg-4">
-				<img src="assets/img/ser03.png" width="180" alt="">
-				<h4>3 - Gather Your Notes</h4>
-				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.</p>
+				<img src="assets/img/cou.png" width="180" alt=""><!--Blog by Oriol Carbonell from The Noun Project-->
+				<h4>3 - Courses You'd Be Intersted In</h4>
+				<form align="left" style="padding-left:25%;">
+				<input type="checkbox" name="anh" value="Film">Arts and Humanities<br>
+				<input type="checkbox" name="meche" value="Music">Mechanical Engineering <br>
+				<input type="checkbox" name="law" value="News">Law <br>
+				<input type="checkbox" name="fashion" value="choral">Fashion <br>
+				<input type="checkbox" name="cse" value="march">Computer Science<br>
+				<input type="checkbox" name="aerosp" value="tel">Aerospace<br>
+				<input type="checkbox" name="engg" value="tech">Engineering <br>
+				<input type="checkbox" name="art" value="fb">Arts <br>
+				<input type="checkbox" name="ba" value="basky">Business Accounting<br>
+				<input type="checkbox" name="lia" value="cri">Library Information Sciences  <br>
+				<input type="checkbox" name="eco" value="pai">Economics <br>
+				<input type="checkbox" name="philo" value="ph">Philosophy <br>
+				<input type="checkbox" name="ca" value="int">Chartered Accountant <br>
+				<input type="checkbox" name="psci" value="ph">Pure Sciences
+				</form>
 
 			</div><!--/col-lg-4 -->
 		</div><!-- /row -->
@@ -105,10 +151,7 @@
 		<div class="row centered">
 			<div class="col-lg-6 col-lg-offset-3">
 				<form class="form-inline" role="form">
-				  <div class="form-group">
-				    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter your email address">
-				  </div>
-				  <button type="submit" class="btn btn-warning btn-lg">Invite Me!</button>
+				  <button type="submit" class="btn btn-primary btn-lg">Yo! Show Me The Way!</button>
 				</form>					
 			</div>
 			<div class="col-lg-3"></div>
@@ -124,7 +167,7 @@
 			</div>
 		</div><!-- /row -->
 	
-		<! -- CAROUSEL -->
+		<!-- CAROUSEL -->
 		<div class="row mt centered">
 			<div class="col-lg-6 col-lg-offset-3">
 				<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
@@ -150,7 +193,7 @@
 				</div>			
 			</div><!-- /col-lg-8 -->
 		</div><!-- /row -->
-	</div><! --/container -->
+	</div><!--/container -->
 	
 	<div class="container">
 		<hr>
